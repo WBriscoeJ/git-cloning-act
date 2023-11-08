9e0224f894aded18f2ea602411202fc28c4fee2e
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-11-08 17:29:15 +0000

    done gitclone activity

---
+++ Wade code project/DocumentionActivivty.py
@@ -0,0 +1,17 @@
+Name of project
+Team/ Engineer name and github link
+Description of your program- what does it do. labeled as description
+Highlight Three (3) features of your program. labeled as features
+Instructions on how to install the program. labeled as installation
+Instructions on how to contribute to the program. labeled as contribute
+Use the following link to format your documentation using markdown.
+
+Markdown getting started. Markdown cheatsheet.
+
+Use the following links to read up on Cesium and Documentation Cesium Documnetation
+
+Please use the name documentationActivity.md as your file name. Please create your activity in the activities folder.
+
+If you do not follow these instructions you will lose points off of the assignment.
+
+This assignment will must be submitted no later than 11:59pm today (11/1/2023) to recieve credit.
